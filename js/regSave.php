818e123e2b937bb8a0024a7c1b115a29245b147f
wj <2996255147@qq.com>
2018-10-17 08:30:03 +0800

    添加了购物车页面，并且实现了五星好评

--- js/regSave.php
+++ js/regSave.php
@@ -1,42 +1,30 @@
 ﻿<?php
-	header("Content-type:text/html;charset=utf-8");
+	header("Content-type:text/html; charset=utf-8");
 	
-	//1、接收数据
-	$username = $_POST['username'];
-	$userpass = $_POST['userpass'];
-	
-	
-	//2、处理（连接数据库，保存数据）
-	//1)、建立连接（搭桥）
-	$con = mysql_connect("localhost","root","root");
+	$name=$_POST["username"];
+	$userpass=$_POST["userpass"];
+
+	$con=mysql_connect("localhost","root","root");
 	if(!$con){
-		echo "连接数据库失败";
-	}else{
-		//2）、选择数据库（目的地）
+		echo "数据库连接失败";
+	}
+	else{
 		mysql_select_db("mydb",$con);
-		
-		//3）、执行SQL数据（运输数据）
-		$sqlstr="select * from user where username='$username'";
+		//1)、查询
+		$sqlstr="select * from user where username='$name'";
 		$result = mysql_query($sqlstr,$con);
 		$rows = mysql_num_rows($result);
-		if($rows>0){				
-			//4）、关闭数据库（过河拆桥）
-			mysql_close($con);
-			echo "亲，注册失败，用户名已经存在";
-		}else{
-			$sqlstr="insert into user(username,userpass) values('$username','$userpass')";
-			$result = mysql_query($sqlstr,$con);	
-			//4）、关闭数据库（过河拆桥）
-			mysql_close($con);
-			
-			//3、响应
-			echo $result."<br/>";
-			if($result){
-
-				header("location:../login.html");
+		if($rows<=0){		
+			//2）、添加	
+			$sqlstr="insert into user values('$name','$userpass')";
+			$result = mysql_query($sqlstr,$con);
+			if($result==1){
+				echo "1";//注册成功；
 			}else{
-				echo "注册失败！";
-			}			
+				echo "0";//注册失败
+			}
+		}else{
+			echo "-1";//注册失败：用户名已经存在，
 		}
 	}
 ?>
